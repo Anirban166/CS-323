b454271d9cc805a879f2daf09415423e5b5d820a
Anirban Chetia <bloodraven166@gmail.com>
2020-04-24 22:02:19 +0530

    Create Plot_12.py

---
+++ Assignments/Basics/Plot_12.py
@@ -0,0 +1,11 @@
+# import matplotlib for plotting/visualization purposes:
+import matplotlib
+
+# importing pyplot from the above library and giving it an alias name of plt
+from matplotlib import pyplot as plt
+
+# plot (x,y) points in the form of ([x-coordinates],[y-coordinates]):
+plt.plot([1,2,3],[4,5,1])
+
+# display plot results:
+plt.show()

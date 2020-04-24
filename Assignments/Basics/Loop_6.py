664932b59231fbaf4d3c04d1bb6efe4b8a5084b4
Anirban Chetia <bloodraven166@gmail.com>
2020-04-24 21:59:51 +0530

    Create Loop_6.py

---
+++ Assignments/Basics/Loop_6.py
@@ -0,0 +1,17 @@
+# While loop
+condition = 1
+# a loop which runs uptil value of variable condition is less than 10:
+while condition < 10:
+	print(condition) # print value
+	condition += 1  # increment value
+# this loop will print numbers from 1 to 10.    
+
+# create a list of integers:
+exampleList = [1,5,6,6,2,1,5,2,1,4]
+# print the elements in the list using a for-loop:
+for x in exampleList:
+    print(x)
+# print elements 1 to (n-1) using a range-based for loop with range(1,n):
+for x in range(1,11):
+    print(x)
+

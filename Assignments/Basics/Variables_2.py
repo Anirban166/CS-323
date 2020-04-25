ba19eb082639872584c806f1d850c028dffa1c27
Anirban Chetia <bloodraven166@gmail.com>
2020-04-25 11:25:45 +0530

    Update Variables_2.py

--- Assignments/Basics/Variables_2.py
+++ Assignments/Basics/Variables_2.py
@@ -12,16 +12,18 @@ print(name)
 
 # multiple assignments :
 a=b=c=1
-# Here, an integer object is created with the value 1,
+# here, an integer object is created with the value 1,
 # and all three variables are assigned to the same memory location.
 
+a,b,c=1,'mousmita','sarma'
 # here ordered assignment takes place, where variables in order
-# are assigned to  their values in the same sequence orderly fashion
+# are assigned to  their values in the same sequence / orderly fashion.
 # a gets assigned the first value after =
 # b gets assigned the second values after =
 # c gets assigned the third value after = 
-# values being comma seperated.
-a,b,c=1,'mousmita','sarma'
+# (values being comma seperated)
+
+# print the values:
 print(a)
 print(b)
 print(c)
@@ -30,3 +32,4 @@ print(c)
 del a
 # if we were to write print(a) here, 
 # it will produce an error declaring that 'a' is not defined
+

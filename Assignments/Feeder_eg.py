bf2daa6749f6ad8a3d881fd9fcd2212b7767c94e
Anirban Chetia <bloodraven166@gmail.com>
2020-04-28 09:05:39 +0530

    Update Feeder_eg.py

--- Assignments/Feeder_eg.py
+++ Assignments/Feeder_eg.py
@@ -1,20 +1,31 @@
+# importing required libraries:
 import numpy as np
-import tensorflow as tf
+import tensorflow.compat.v1 as tf
+# disabling active eager execution:
+tf.compat.v1.disable_eager_execution()
 
-# Define a placeholder that expects a vector of three floating-point values,
-# and a computation that depends on it.
+# creating a placeholder x that expects a vector of three floating-point 
+# values, with a computation that depends on it:
 x = tf.placeholder(tf.float32, shape=[3])
+# using tf.math.square() to assign the square of x to a variable:
 y = tf.square(x)
-
+# note that in the above line, the argument's (x) data type is not specified,
+# but it is inferred from the value - i.e. x being a square of a float32 or
+# a 32-bit floating point, will remain a float32.
+ 
+# creating a session and initializing our variables: 
 with tf.Session() as sess:
-  # Feeding a value changes the result that is returned when you evaluate `y`.
-  print(sess.run(y, {x: [1.0, 2.0, 3.0]}))  # => "[1.0, 4.0, 9.0]"
-  print(sess.run(y, {x: [0.0, 0.0, 5.0]}))  # => "[0.0, 0.0, 25.0]"
-
-  # Raises `tf.errors.InvalidArgumentError`, because you must feed a value for
-  # a `tf.placeholder()` when evaluating a tensor that depends on it.
-  # sess.run(y)
-
-  # Raises `ValueError`, because the shape of `37.0` does not match the shape
-  # of placeholder `x`.
-  # sess.run(y, {x: 37.0})
+  # Feeding a value changes the result that is returned when we evaluate `y`:
+  print(sess.run(y, {x: [1.0, 2.0, 3.0]}))  # => "[1.0, 4.0, 9.0]" (squared)
+  print(sess.run(y, {x: [0.0, 0.0, 5.0]}))  # => "[0.0, 0.0, 25.0]" (squared)
+"""
+Now, if we write:
+sess.run(y)
+It will raise a `tf.errors.InvalidArgumentError`, because we must feed a value 
+for a `tf.placeholder()` when evaluating a tensor that depends on it.
+--------------------------------------------------------------------------------
+Again, if we were to write:
+sess.run(y, {x: 37.0})
+It will raise `ValueError`, because the shape of `37.0` does not match the 
+shape of our placeholder `x`.
+"""
